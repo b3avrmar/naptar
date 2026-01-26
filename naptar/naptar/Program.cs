db7bb5f0f0af02b023596f2c32013eaed09f2707
b3avrmar <b3avrmar@vasvari.org>
2026-01-26 09:10:56 +0100

    Menu options

--- naptar/naptar/Program.cs
+++ naptar/naptar/Program.cs
@@ -35,6 +35,64 @@ namespace naptar
             }
         }
 
+        static void Menu()
+        {
+            Console.WriteLine("1. Naptár megjelenítése");
+            Console.WriteLine("2. Esemény hozzáadása");
+            Console.WriteLine("3. Legközelebbi esemány megjelenítése");
+            Console.WriteLine("4. Kilépés");
+
+            Console.Write("Válasszon egy opciót: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            switch (input)
+            {
+                case 1:
+                    Naptar_megjelenites();
+                    break;
+                case 2:
+                    Esemeny_hozzaadas();
+                    break;
+                case 3:
+                    Legkozelebbi_esemeny();
+                    break;
+                case 4:
+                    Kilepes();
+                    break;
+                default:
+                    Console.WriteLine("Hibás opció, próbálja újra.");
+                    Console.ReadKey();
+                    Console.Clear();
+                    Menu();
+                    break;
+            }
+        }
+        static void Naptar_megjelenites()
+        {
+            Console.WriteLine("Naptár megjelenítése");
+        }
+
+        static void Esemeny_hozzaadas()
+        {
+            Console.WriteLine("Esemény hozzáadása");
+        }
+
+        static void Legkozelebbi_esemeny()
+        {
+            Console.WriteLine("Legközelebbi esemény megjelenítése");
+        }
+
+        static void Kilepes()
+        {
+            Mentes();
+            Console.WriteLine("Kilépés");
+        }
+
+        static void Mentes()
+        {
+            Console.WriteLine("Adatok mentése");
+        }
+
         public struct Datetime
         {
             
