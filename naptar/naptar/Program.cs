576ee1a5e2746c976da7f7d15dbee91feee200a4
b3avrmar <b3avrmar@vasvari.org>
2026-02-08 22:16:10 +0100

    Bugfix

--- naptar/naptar/Program.cs
+++ naptar/naptar/Program.cs
@@ -133,11 +133,26 @@ namespace naptar
             Console.Write("Add meg space-el elválasztva az esemény nevét, dátumát, időtartamát, leírását és a felhasználó nevét: ");
             string[] input = Console.ReadLine().Split(' ');
 
+            DateTime datum = DateTime.Parse(input[1]);
+            TimeSpan idotartam = TimeSpan.Parse(input[2]);
+
+            if (datum.Hour < 8 || datum.Hour >= 20)
+            {
+                Console.WriteLine("Hiba: Az esemény csak reggel 8 és este 20 óra között vehető fel!");
+                return esemenyek;
+            }
+
+            if (idotartam < TimeSpan.FromMinutes(30) || idotartam > TimeSpan.FromHours(2))
+            {
+                Console.WriteLine("Hiba: Az esemény időtartama csak 30 perc és 2 óra között lehet!");
+                return esemenyek;
+            }
+
             Esemeny ujEsemeny = new Esemeny
             {
                 Nev = input[0],
-                Datum = DateTime.Parse(input[1]),
-                Idotartam = TimeSpan.Parse(input[2]),
+                Datum = datum,
+                Idotartam = idotartam,
                 Leiras = input[3],
                 Felhasznalo = input[4]
             };
